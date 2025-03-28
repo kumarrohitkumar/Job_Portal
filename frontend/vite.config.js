96c6751a1692680b754526fd73144005d7f51a5a
This is Rohit <rk8091267@gmail.com>
2025-03-28 17:34:44 +0530

    fix 2 for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    outDir: 'dist'  // Ensure this line exists
+  },
   plugins: [react()],
   resolve: {
     alias: {
