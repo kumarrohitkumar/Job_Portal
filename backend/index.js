5ea4b68c2cf21d2aaef5fcd1cb4a643fd10b93da
This is Rohit <rk8091267@gmail.com>
2025-04-12 21:15:41 +0530

    this is

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // Dynamically Set CORS Based on Environment
 const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? "https://job-portal-2-c30j.onrender.com"
-      : "http://localhost:5173", // Local Frontend
+  origin: "http://localhost:5173", // Local Frontend
   credentials: true,
 };
 app.use(cors(corsOptions));
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,23 +1,15 @@
 // You need to define the mode correctly, for example, from environment variables
-const mode =  ""; // Defaulting to development
 
 let USER_API_END_POINT,
   JOB_API_END_POINT,
   APPLICATION_API_END_POINT,
   COMPANY_API_END_POINT;
 
-if (mode === "production") {
-  USER_API_END_POINT = "https://job-portal-2-c30j.onrender.com/api/v1/user";
-  JOB_API_END_POINT = "https://job-portal-2-c30j.onrender.com/api/v1/job";
-  APPLICATION_API_END_POINT =
-    "https://job-portal-2-c30j.onrender.com/api/v1/application";
-  COMPANY_API_END_POINT = "https://job-portal-2-c30j.onrender.com/api/v1/company";
-} else {
   USER_API_END_POINT = "http://localhost:8000/api/v1/user"; // Assuming local dev API
   JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
   APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
   COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
-}
+
 
 // Export the variables
 export {
