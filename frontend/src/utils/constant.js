cd8b986f319deada37738638ecb49e8305dae23e
This is Rohit <rk8091267@gmail.com>
2025-04-12 21:31:16 +0530

    final

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,14 +1,11 @@
 // You need to define the mode correctly, for example, from environment variables
 
-let USER_API_END_POINT,
-  JOB_API_END_POINT,
-  APPLICATION_API_END_POINT,
-  COMPANY_API_END_POINT;
 
-  USER_API_END_POINT = "http://localhost:8000/api/v1/user"; // Assuming local dev API
-  JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-  APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-  COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+
+  const USER_API_END_POINT = "http://localhost:8000/api/v1/user"; // Assuming local dev API
+  const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+  const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
+  const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
 
 
 // Export the variables
