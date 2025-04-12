a3edadcf0f0ff39a4788e101fedee551e2c947cd
This is Rohit <rk8091267@gmail.com>
2025-04-12 20:13:14 +0530

    link update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? "https://job-portal-ckeu.onrender.com" // Production API
+      ? "https://job-portal-2-c30j.onrender.com" // Production API
       : "http://localhost:5173", // Local Frontend
   credentials: true,
 };
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -7,11 +7,11 @@ let USER_API_END_POINT,
   COMPANY_API_END_POINT;
 
 if (mode === "production") {
-  USER_API_END_POINT = "https://job-portal-ckeu.onrender.com/api/v1/user";
-  JOB_API_END_POINT = "https://job-portal-ckeu.onrender.com/api/v1/job";
+  USER_API_END_POINT = "https://job-portal-2-c30j.onrender.com/api/v1/user";
+  JOB_API_END_POINT = "https://job-portal-2-c30j.onrender.com/api/v1/job";
   APPLICATION_API_END_POINT =
-    "https://job-portal-ckeu.onrender.com/api/v1/application";
-  COMPANY_API_END_POINT = "https://job-portal-ckeu.onrender.com/api/v1/company";
+    "https://job-portal-2-c30j.onrender.com/api/v1/application";
+  COMPANY_API_END_POINT = "https://job-portal-2-c30j.onrender.com/api/v1/company";
 } else {
   USER_API_END_POINT = "http://localhost:8000/api/v1/user"; // Assuming local dev API
   JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
